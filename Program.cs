6390a5fe6819033713776afbe12dbb7580e13e8b
AbdulazizCoding <abdulazizabduqaxxarov04@gmail.com>
2022-07-16 21:40:57 +0500

    Added some packages

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
+using Telegram.Bot;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var token = builder.Configuration.GetValue("BotToken", string.Empty);
+
+builder.Services.AddSingleton(new TelegramBotClient(token));
+
 var app = builder.Build();
+
 
 app.Run();
