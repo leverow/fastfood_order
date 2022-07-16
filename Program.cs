5e212e1a4e18863b052ba97b7d3d7fe1c0a42204
AbdulazizCoding <abdulazizabduqaxxarov04@gmail.com>
2022-07-16 23:35:42 +0500

    Added BotBackground Service

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using fastfood_order.Services;
 using Telegram.Bot;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 var token = builder.Configuration.GetValue("BotToken", string.Empty);
 
 builder.Services.AddSingleton(new TelegramBotClient(token));
+builder.Services.AddHostedService<BotBackgroundService>();
 
 var app = builder.Build();
 
