2d386ac8673c0314e5f426ab8184ca6d09a54658
starscriptuz <info.enjoymedia@mail.ru>
2022-07-16 22:20:09 +0500

    Added Background Service

---
+++ Services/BotBackgroundService.cs
@@ -0,0 +1,8 @@
+namespace fastfood_order.Services;
+public class BotBackgroundService : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
