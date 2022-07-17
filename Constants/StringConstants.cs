9f4227b6d65129ccbefed2241a6628e97d560b0c
starscriptuz <info.enjoymedia@mail.ru>
2022-07-17 22:53:45 +0500

    Language buttons added

---
+++ Constants/StringConstants.cs
@@ -0,0 +1,11 @@
+namespace bot.Constants;
+
+public static class StringConstants
+{
+    public static Dictionary<string, string> LanguageNames => new()
+    {
+        { "uz-Uz", "O'zbekcha" },
+        { "ru-Ru", "Русский" },
+        { "en-Us", "English" },
+    };
+}
---
+++ Helpers/MarkupHelpers.cs
@@ -0,0 +1,33 @@
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace bot.Helpers;
+
+public static class MarkupHelpers
+{
+    public static InlineKeyboardMarkup GetInlineKeyboardMatrix(Dictionary<string, string> keys, int columns = 2)
+    {
+        int row = 0;
+
+        var buttonMatrix = new List<List<InlineKeyboardButton>>();
+
+        while(keys.Skip(row).Take(columns)?.Count() > 0)
+        {
+            var buttons = keys.Skip(row * columns).Take(columns).Select(k => InlineKeyboardButton.WithCallbackData(k.Value, k.Key)).ToList();
+
+            buttonMatrix.Add(buttons);
+            
+            row++;
+        }
+
+        return new InlineKeyboardMarkup(buttonMatrix.ToArray());
+    }
+
+    public static ReplyKeyboardMarkup GetReplyKeyboardMarkup(string[] keys, int columns = 2)
+    {
+        var buttons = keys.Select(k => new KeyboardButton(k));
+        return new ReplyKeyboardMarkup(buttons)
+        {
+            ResizeKeyboard = true
+        };
+    }
+}
